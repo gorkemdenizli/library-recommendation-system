ac74459fa1e800b39db5ff73be64210c6f80450a
Semih Görkem Denizli <gorkemdenizli@gmail.com>
2026-01-02 21:45:56 +0300

    fixes wip 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,28 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Amplify } from 'aws-amplify';
+
 import './index.css';
 import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
-
-import { Amplify } from 'aws-amplify';
+console.log('ENV CHECK', {
+  region: import.meta.env.VITE_AWS_REGION,
+  userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
+  clientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
+});
 
 Amplify.configure({
   Auth: {
     Cognito: {
       userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
       userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
+      // Önerilen: region ekle
     },
   },
 });
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
