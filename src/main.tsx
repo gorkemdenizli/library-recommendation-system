78269fa2698a35e6d8b86762c86c653ce5137ffb
Semih Görkem Denizli <gorkemdenizli@gmail.com>
2026-01-02 21:49:17 +0300

    fixes wip 2

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import './index.css';
 import App from './App.tsx';
 
 console.log('ENV CHECK', {
-  region: import.meta.env.VITE_AWS_REGION,
   userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
   clientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
 });
@@ -16,7 +15,6 @@ Amplify.configure({
     Cognito: {
       userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
       userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
-      // Önerilen: region ekle
     },
   },
 });
